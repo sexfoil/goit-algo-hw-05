1ca5075ac520289c6567a2e7356e2a33a1656724
Viacheslav_Poliakov <Viacheslav_Poliakov@epam.com>
2025-02-07 23:18:49 +0200

    Finish task 1

---
+++ tasks/task1.py
@@ -0,0 +1,56 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
+
+    def hash_function(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        key_hash = self.hash_function(key)
+        key_value = [key, value]
+
+        if self.table[key_hash] is None:
+            self.table[key_hash] = list([key_value])
+            return True
+        else:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.table[key_hash].append(key_value)
+            return True
+
+    def get(self, key):
+        key_hash = self.hash_function(key)
+        if self.table[key_hash] is not None:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+    # Моя реалізація
+    def delete(self, key):
+        key_hash = self.hash_function(key)
+        if self.table[key_hash] is not None:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    value = pair[1]
+                    self.table[key_hash].remove(pair)
+                    return value
+        return None
+
+# Тестуємо нашу хеш-таблицю:
+H = HashTable(5)
+H.insert("apple", 10)
+H.insert("orange", 20)
+H.insert("banana", 30)
+
+print(H.get("apple"))   # Виведе: 10
+print(H.get("orange"))  # Виведе: 20
+print(H.get("banana"))  # Виведе: 30
+
+# Перевірка моєї реалізації
+print(H.delete("Orange")) # Немає такого елемента - має повернути None
+print(H.delete("orange")) # Має видалити елемент та повернути значення 20
+print(H.delete("orange")) # Вже немає такого елемента - має повернути None
